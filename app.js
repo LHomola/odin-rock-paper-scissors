ae57427e5d1154260f2f20780bf11b52df397fa5
LHomola <lhomola83@gmail.com>
2024-04-18 22:23:16 +0100

    add comments in app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function getPlayerChoice() {
 }
 
 // function that plays a single round of Rock Paper Scissors
-// function takes two parameters (playerSelection and computerSelection) and returns a string that declares the winner or tie of the round
+// function takes two parameters (playerSelection and computerSelection) and returns a number representing the result (1: win, 0: tie, -1 loss)
 function playRound(playerSelection, computerSelection) {
     console.log(`Computer picked: %s`, computerSelection);
     console.log(`You picked: %s`, playerSelection);
@@ -67,24 +67,26 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+// play given number of rounds of the game
 function playGame() {
     let gameResult;
     let wins = 0;
     let losses = 0;
     const rounds = 5; // number of games to be played
 
-    for (let i = 0; i < rounds; i++) {
+    for (let i = 0; i < rounds; i++) { // loop 'rounds' number of times
         const computerSelection = getComputerChoice();
         const playerSelection = getPlayerChoice();
 
-        gameResult = playRound(playerSelection, computerSelection);
-        if (gameResult === 1) {
+        gameResult = playRound(playerSelection, computerSelection); // get result of each game
+        if (gameResult === 1) { // if win
             wins++;
-        } else if (gameResult === -1) {
+        } else if (gameResult === -1) { // if loss
             losses++;
         }
     }
 
+    // print results to the console
     console.log(`Wins: %d`, wins);
     console.log(`Losses: %d`, losses);
     if (wins > losses) {        
