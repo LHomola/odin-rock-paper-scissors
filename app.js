e0ddbf758b912f858553fff74250a347c9b3b279
LHomola <lhomola83@gmail.com>
2024-04-18 22:18:07 +0100

    add playGame()
    use playRound function to play a five round game that keeps score and reports a winner or loser at the end

--- app.js
+++ app.js
@@ -7,38 +7,6 @@ function getComputerChoice() {
     return Math.floor(Math.random() * 3 + 1);
 }
 
-// function that plays a single round of Rock Paper Scissors
-// function takes two parameters (playerSelection and computerSelection) and returns a string that declares the winner or tie of the round
-function playRound(playerSelection, computerSelection) {
-    console.log(`Computer picked: %s`, computerSelection);
-    console.log(`You picked: %s`, playerSelection);
-    if (playerSelection === SCISSORS) {
-        if (computerSelection === SCISSORS) {
-            return "Draw! Scissors and Scissors"
-        } else if (computerSelection === PAPER) {
-            return "You Win! Scissors beats Paper"
-        } else {
-            return "You Lose! Rock beats Scissors";
-        }
-    } else if (playerSelection === PAPER) { // paper
-        if (computerSelection === SCISSORS) {
-            return "You Lose! Scissors beats Paper"
-        } else if (computerSelection === PAPER) {
-            return "Draw! Paper and Paper"
-        } else {
-            return "You Win! Paper beats Rock";
-        }
-    } else { // rock
-        if (computerSelection === SCISSORS) {
-            return "You Win! Rock beats Scissors";
-        } else if (computerSelection === PAPER) {
-            return "You Lose! Paper beats Rock"
-        } else {
-            return "Draw! Rock and Rock"
-        }
-    }
-}
-
 // prompt user for their choice and convert to an int (1, 2, 3)
 function getPlayerChoice() {
     let choice = 0;
@@ -58,8 +26,74 @@ function getPlayerChoice() {
     return choice;
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = getPlayerChoice();
+// function that plays a single round of Rock Paper Scissors
+// function takes two parameters (playerSelection and computerSelection) and returns a string that declares the winner or tie of the round
+function playRound(playerSelection, computerSelection) {
+    console.log(`Computer picked: %s`, computerSelection);
+    console.log(`You picked: %s`, playerSelection);
+    if (playerSelection === SCISSORS) {
+        if (computerSelection === SCISSORS) {
+            console.log("Tie! Scissors and Scissors");
+            return 0;
+        } else if (computerSelection === PAPER) {
+            console.log("You Win! Scissors beats Paper");
+            return 1;
+        } else {
+            console.log("You Lose! Rock beats Scissors");
+            return -1;
+        }
+    } else if (playerSelection === PAPER) { // paper
+        if (computerSelection === SCISSORS) {
+            console.log("You Lose! Scissors beats Paper");
+            return -1;
+        } else if (computerSelection === PAPER) {
+            console.log("Tie! Paper and Paper");
+            return 0;
+        } else {
+            console.log("You Win! Paper beats Rock");
+            return 1;
+        }
+    } else { // rock
+        if (computerSelection === SCISSORS) {
+            console.log("You Win! Rock beats Scissors");
+            return 1;
+        } else if (computerSelection === PAPER) {
+            console.log("You Lose! Paper beats Rock");
+            return -1;
+        } else {
+            console.log("Tie! Rock and Rock");
+            return 0;
+        }
+    }
+}
 
-// print result of game to console
-console.log(playRound(playerSelection, computerSelection));
+function playGame() {
+    let gameResult;
+    let wins = 0;
+    let losses = 0;
+    const rounds = 5; // number of games to be played
+
+    for (let i = 0; i < rounds; i++) {
+        const computerSelection = getComputerChoice();
+        const playerSelection = getPlayerChoice();
+
+        gameResult = playRound(playerSelection, computerSelection);
+        if (gameResult === 1) {
+            wins++;
+        } else if (gameResult === -1) {
+            losses++;
+        }
+    }
+
+    console.log(`Wins: %d`, wins);
+    console.log(`Losses: %d`, losses);
+    if (wins > losses) {        
+        console.log("You win!")
+    } else if (wins < losses) {
+        console.log("You lose!")
+    } else {
+        console.log("Tie!")
+    }
+}
+
+playGame();
