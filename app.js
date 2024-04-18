569119d5ef535d953058e2629af29d2d894e13fc
LHomola <lhomola83@gmail.com>
2024-04-18 21:16:22 +0100

    update getComputerChoice()
    - remove check if generated number is within correct range
    - simply return the generated value

--- app.js
+++ app.js
@@ -2,13 +2,7 @@
 
 // generate random integer between 1 and 3 and return the value
 function getComputerChoice() {
-    randInt = Math.floor(Math.random() * 3 + 1);
-    if (randInt >= 1 && randInt <= 3) {
-        return randInt; // return random value
-    } else {
-        alert("Random number generation error");
-        return randInt; // if value not 1, 2, 3 show error message;
-    }
+    return Math.floor(Math.random() * 3 + 1);
 }
 
 function getPlayerChoice() {
