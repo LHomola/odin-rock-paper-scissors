d823fc73cc1aa75648eab04dfb8ae978230b2e05
LHomola <lhomola83@gmail.com>
2024-04-18 21:01:53 +0100

    amend getComputerChoice()
    return generated int instead of string for easier comparison

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
+// values assigned to options: scissors = 1, paper = 2, rock = 3
+
+// generate random integer between 1 and 3 and return the value
 function getComputerChoice() {
     randInt = Math.floor(Math.random() * 3 + 1);
-    if (randInt === 1) {
-        return "Rock";
-    } else if (randInt === 2) {
-        return "Paper";
-    } else if (randInt === 3) {
-        return "Scissors";
+    if (randInt >= 1 && randInt <= 3) {
+        return randInt; // return random value
     } else {
-        console.log("Random number generation error")
-        return;
+        alert("Random number generation error");
+        return randInt; // if value not 1, 2, 3 show error message;
     }
 }
-
-console.log(getComputerChoice());
