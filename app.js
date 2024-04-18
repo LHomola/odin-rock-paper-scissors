f423954e76e7600de5690a44e97197bad3b6537e
LHomola <lhomola83@gmail.com>
2024-04-18 21:47:51 +0100

    add playRound()
    plays a single round of Rock Paper Scissors -> returns a string that declares the winner or tie of the round

--- app.js
+++ app.js
@@ -5,11 +5,44 @@ function getComputerChoice() {
     return Math.floor(Math.random() * 3 + 1);
 }
 
+// function that plays a single round of Rock Paper Scissors
+// function takes two parameters (playerSelection and computerSelection) and returns a string that declares the winner or tie of the round
+function playRound(playerSelection, computerSelection) {
+    console.log(`Computer picked: %s`, computerSelection);
+    console.log(`You picked: %s`, playerSelection);
+    if (playerSelection === 1) { // scissors
+        if (computerSelection === 1) {
+            return "Draw! Scissors and Scissors"
+        } else if (computerSelection === 2) {
+            return "You Lose! Scissors beats Paper"
+        } else {
+            return "You Win! Scissors beats Rock";
+        }
+    } else if (playerSelection === 2) { // paper
+        if (computerSelection === 1) {
+            return "You Lose! Scissors beats Paper"
+        } else if (computerSelection === 2) {
+            return "Draw! Paper and Paper"
+        } else {
+            return "You Win! Paper beats Rock";
+        }
+    } else { // rock
+        if (computerSelection === 1) {
+            return "You Win! Rock beats Scissors";
+        } else if (computerSelection === 2) {
+            return "You Lose! Paper beats Rock"
+        } else {
+            return "Draw! Rock and Rock"
+        }
+    }
+}
+
+// prompt user for their choice and convert to an int (1, 2, 3)
 function getPlayerChoice() {
-    const choice = 0;
+    let choice = 0;
     // get user choice
     while (choice === 0) {
-        const choiceStr = prompt("Enter your choice (rock, paper, scissors): ").toLocaleLowerCase;
+        const choiceStr = prompt("Enter your choice (rock, paper, scissors): ").toLowerCase();
         // convert user selection to an int    
         if (choiceStr === "rock") {
             choice = 3;
@@ -22,3 +55,9 @@ function getPlayerChoice() {
 
     return choice;
 }
+
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+
+// print result of game to console
+console.log(playRound(playerSelection, computerSelection));
